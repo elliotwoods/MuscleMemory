43eb91307c212450837d04c96657774e53970705
Elliot Woods <elliot.woods@gmail.com>
2020-10-01 12:42:06 +0900

    add sketches

--- Sketches/ESP-IDF Devices/src/AS5047.h
+++ Sketches/ESP-IDF Devices/src/AS5047.h
@@ -3,9 +3,21 @@
 #include "driver/gpio.h"
 
 class AS5047 {
+    static uint16_t calcParity(uint16_t);
 public:
+    struct Errors {
+        bool hasErrors = false;
+        bool framingError = false;
+        bool invalidCommand = false;
+        bool parityError = false;
+    };
+
     AS5047();
     uint16_t getPosition();
+    Errors getErrors();
 protected:
-    spi_host_device_t spiHostDevice;
+    uint16_t readRegister(uint16_t request);
+    uint16_t parseResponse(uint16_t); ///< Returns the value and sets the local error bit
+    spi_device_handle_t deviceHandle;
+    bool hasError = false;
 };
