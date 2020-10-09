6605fae1d096989669651b8095b97a02166d26e9
Elliot Woods <elliot.woods@gmail.com>
2020-10-09 19:09:24 +0900

    encoder calibration progress

--- Sketches/ESP-IDF Devices/src/EncoderCalibration.h
+++ Sketches/ESP-IDF Devices/src/EncoderCalibration.h
@@ -5,21 +5,26 @@
 
 class EncoderCalibration {
 public:
-    struct Settings {
-        Settings() { }
-        uint16_t stepsPerRevolution = 360 / 1.8;
-        uint8_t current = 64;
-        uint8_t cycles = 8;
+	struct Settings {
+		Settings() { }
+		uint16_t stepsPerRevolution = 360 / 1.8;
+		uint8_t current = 64;
+		uint8_t cycles = 8;
 
-        /// The amount of time to hold the step for before taking a reading
-        /// Note : this also affects the speed of the calibration routine
-        uint16_t stepHoldTimeMS = 10;
-        uint16_t pauseTimeBetweenCyclesMS = 500;
-    };
+		/// The amount of time to hold the step for before taking a reading
+		/// Note : this also affects the speed of the calibration routine
+		uint16_t stepHoldTimeMS = 50;
+		uint16_t pauseTimeBetweenCyclesMS = 500;
+	};
 
-    void calibrate(AS5047 &
-        , MotorDriver &
-        , const Settings & settings = Settings());
+	void calibrate(AS5047 &
+		, MotorDriver &
+		, const Settings & settings = Settings());
 private:
-    Settings settings;
+	void recordStep(uint16_t stepIndex
+		, AS5047 & encoder
+		, MotorDriver & motorDriver
+		, uint32_t * accumulatedEncoderValue
+		, uint8_t * visitsPerStep);
+	Settings settings;
 };
