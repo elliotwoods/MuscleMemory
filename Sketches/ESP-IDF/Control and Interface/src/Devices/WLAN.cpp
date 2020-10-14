7224fce527cd293359861393483bae50aae3f7a3
Elliot Woods <elliot.woods@gmail.com>
2020-10-14 19:49:57 +0900

    add wifi credentials

---
+++ Sketches/ESP-IDF/Control and Interface/src/Devices/WLAN.cpp
@@ -0,0 +1,18 @@
+#include "WLAN.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/ringbuf.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
+namespace WLAN {
+	//----------
+	WLAN::WLAN()
+	{
+		
+	}
+}
