122c079449fd339018390eb4b5f6c7c5cdd2e070
Jin <ql.electricgraphy@gmail.com>
2020-10-15 13:03:52 +0900

    - change the folder name - Add UART basic communication part

--- Sketches/ESP-IDF/Screen and CAN/src/main.cpp
+++ Sketches/ESP-IDF/Screen and CAN/src/main.cpp
@@ -14,6 +14,79 @@
 #include "Panels/RegisterList.h"
 #include "Dial.h"
 
+#define DEVICEID	0
+
+// Set device ID ---------------------------
+typedef uint16_t DeviceID;
+DeviceID device = DEVICEID;
+
+// UART communication part ---------------------------
+// Only for the router device(0) 
+#if	DEVICEID == 0
+	#include <stdio.h>
+	#include <string.h>
+	#include <stdlib.h>
+	#include "esp_log.h"
+	#include "nvs_flash.h"
+	#include "driver/uart.h"
+	#include "soc/uart_struct.h"
+
+	#define ECHO_TEST_TXD  (1)
+	#define ECHO_TEST_RXD  (3)
+	#define BUF_SIZE (1024)
+
+	//UART communication part
+	void UART_communication(void *pvParameter)
+	{    
+		const uart_port_t uart_num = UART_NUM_0;
+		uart_config_t uart_config = {
+			.baud_rate = 115200,
+			.data_bits = UART_DATA_8_BITS,
+			.parity = UART_PARITY_DISABLE,
+			.stop_bits = UART_STOP_BITS_1,
+			.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,    
+			.rx_flow_ctrl_thresh = 122,
+		};
+		//Configure UART1 parameters
+		uart_param_config(uart_num, &uart_config);
+	
+		//Set UART1 pins(TX: IO4, RX: I05)
+		uart_set_pin(uart_num, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+		
+		//Install UART driver (we don't need an event queue here)
+		//In this example we don't even use a buffer for sending data.
+		uart_driver_install(uart_num, BUF_SIZE * 2, 0, 0, NULL, 0);
+	
+		uint8_t* data = (uint8_t*) malloc(BUF_SIZE);
+		while(1) {
+			//Read data from UART
+			int len = uart_read_bytes(uart_num
+				, data
+				, BUF_SIZE
+				, ((1000/60)/2) / portTICK_RATE_MS);
+			data[len] = '\0';
+
+			if(len==8){
+				printf("msg length: %d \n", len);
+				//Write data back to UART
+				uint8_t _targetID = (uint8_t)data[0];
+				uint8_t _operation = (uint8_t)data[1];
+				uint16_t _registerID = ((uint16_t)data[3]<<8) + (uint16_t)data[2];
+				uint32_t _value = ((uint32_t)data[7]<<24) 
+								+ ((uint32_t)data[6]<<16)
+								+ ((uint32_t)data[5]<<8)
+								+ (uint32_t)data[4];
+				printf("%u,%u,%u,%u\n",_targetID,_operation,_registerID,_value);
+				//printf("%u,%u,%u,%u,%u,%u,%u,%u\n",(uint8_t)data[0],(uint8_t)data[1],(uint8_t)data[2],(uint8_t)data[3],(uint8_t)data[4],(uint8_t)data[5],(uint8_t)data[6],(uint8_t)data[7]);
+				//uart_write_bytes(uart_num, (const uint8_t) data, len);
+			}else{
+				// wrong msg format
+			}
+			vTaskDelay(10 / portTICK_RATE_MS);
+		}
+	}	
+#endif
+
 extern "C"
 {
 #include <u8g2_esp32_hal.h>
@@ -22,18 +95,11 @@ extern "C"
 // Rotary Encoder part
 Dial dial;
 
-
-
-
-U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, 15, 4, 16);
-
-// Set device ID ---------------------------
-typedef uint16_t DeviceID;
-DeviceID device = 1;
-
 //-----------------------------------------------------------------
 
 // OLED Part -----------------------------------------------------
+U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, 15, 4, 16);
+
 uint8_t registerySize = Registry::X().registers.size();
 uint8_t maxRows = 4;
 uint8_t totalPages = registerySize / maxRows + (registerySize % maxRows>0);
@@ -64,7 +130,7 @@ void transmitting(uint16_t targetID
 	// 	|	0	|	Target ID
 	// 	|	1	|	Operation
 	// 	|  2,3 	|	Register ID
-	// 	|4,5,6,7|	Arguments
+	// 	|4,5,6,7|	Value
 
 	message.data_length_code = 8;
 	auto dataMover = message.data;
@@ -149,7 +215,9 @@ void receiving(void *pvParameter)
 
 void setup()
 {
-	Serial.begin(115200);
+	#if	DEVICEID != 0
+		Serial.begin(115200);
+	#endif
 
 	// set Rotary encoder
 	dial.init(gpio_num_t::GPIO_NUM_34, gpio_num_t::GPIO_NUM_35);
@@ -194,10 +262,13 @@ void setup()
 		return;
 	}
 
+	// start UART receiving Task
+	#if	DEVICEID == 0
+		xTaskCreate(UART_communication, "uart_echo_task", 2048, NULL, 10, NULL); 
+	#endif
 
-	xTaskCreate(&receiving, "RECEIVING", 2048, NULL, 5, NULL);	// start CAN receiving Task
-
-	
+	// start CAN receiving Task
+	xTaskCreate(&receiving, "RECEIVING", 2048, NULL, 10, NULL);	
 }
 
 uint32_t _msg = 100;
