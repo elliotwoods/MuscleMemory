0b6d5c35a628e6b9733d1c88be3606224fcca42c
Elliot Woods <elliot.woods@gmail.com>
2020-10-15 20:47:19 +0900

    ready to implement agent

--- Sketches/ESP-IDF/ControlAndInterface/src/DataTypes.h
+++ Sketches/ESP-IDF/ControlAndInterface/src/DataTypes.h
@@ -5,7 +5,10 @@
 typedef uint16_t EncoderReading;
 typedef uint8_t PositionWithinStepCycle;
 typedef uint16_t PositionWithinShaftCycle;
-typedef int32_t Position;
-typedef float ShaftCycle;
+typedef int32_t MultiTurnPosition;
+typedef int32_t Velocity;
+typedef int16_t Turns;
 typedef int8_t Torque;
+typedef uint16_t Current;
+typedef uint16_t Frequency; // Update loop frequency [Hz]
 
