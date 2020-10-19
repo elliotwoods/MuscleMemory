abb2c96a14f1776f9cb3eb3ed0b9b70e03755182
Jin <ql.electricgraphy@gmail.com>
2020-10-19 14:14:56 +0900

    Working on GUI graphics

--- Sketches/ESP-IDF/ControlAndInterface/src/GUI/Panels/Logo.h
+++ Sketches/ESP-IDF/ControlAndInterface/src/GUI/Panels/Logo.h
@@ -1,31 +1,4 @@
-#pragma once
-
-#include "Panel.h"
-#include <memory>
-#include <vector>
-
-class GuiController {
-public:
-    static GuiController & X();
-    void init(U8G2 & screen, std::shared_ptr<Panel> rootPanel);
-    void update();
-
-    void dialButtonPressed();
-    void dialTurned(int8_t);
-private:
-    GuiController() {}
-    std::shared_ptr<Panel> currentPanel = nullptr;
-    std::vector<std::shared_ptr<Panel>> viewStack;
-
-    // This will be shown when everything else exits
-    std::shared_ptr<Panel> rootPanel;
-    U8G2 * screen;
-    
-    int8_t dialMovements = 0;
-    bool isDialButtonPressed = false;
-};
-
-
+// Kimchi and Chips LOGO *XBM 
 #define KimchipsLogo100_width 100
 #define KimchipsLogo100_height 39
 
