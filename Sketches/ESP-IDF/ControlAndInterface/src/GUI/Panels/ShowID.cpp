a247c46e1176cfb84504a1fb3ce3f3b4c8dad0fe
Jin <ql.electricgraphy@gmail.com>
2020-10-20 11:55:02 +0900

    Add GUI { Splash Scree, ShowID, DashBoard }

--- Sketches/ESP-IDF/ControlAndInterface/src/GUI/Panels/ShowID.cpp
+++ Sketches/ESP-IDF/ControlAndInterface/src/GUI/Panels/ShowID.cpp
@@ -21,7 +21,21 @@ namespace GUI {
 		ShowID::draw(U8G2& u8g2)
 		{
 			auto deviceID = Registry::X().registers.at(Registry::RegisterType::DeviceID);
+			
+			u8g2.setFont(u8g2_font_nerhoe_tr ); 
+			char message[200];
+			sprintf(message, "DEVICE");
+			u8g2.drawStr(20, 35, message);	
 
+			u8g2.setFont(u8g2_font_fub35_tn);
+			sprintf(message, "%d", deviceID.value);
+			int IDxPosition = 75;
+			if(deviceID.value>99){
+				IDxPosition+=20;
+			}else if(deviceID.value>9){
+				IDxPosition+=10;
+			}
+			u8g2.drawStr(IDxPosition, 50, message);
 		}
 
 		
