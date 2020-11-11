eb55aa08ffbf27902629942840f99cc14267b6e3
Elliot Woods <elliot.woods@gmail.com>
2020-11-11 18:10:55 +0900

    CANResonder implement filtering fixes #60 Increase Rx buffer size to 64 (default is 5 which saturated, especially without filter)

--- Firmware/src/Interface/CANResponder.h
+++ Firmware/src/Interface/CANResponder.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <stdint.h>
+
 namespace Interface {
 	class CANResponder {
 	public:
 		void init();
+		void deinit();
 		void update();
+	private:
+		uint16_t deviceIDMask;
 	};
 }
