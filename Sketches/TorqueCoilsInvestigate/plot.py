593780105f848654b6e58e519f93747bdbeb0103
Elliot Woods <elliot.woods@gmail.com>
2020-11-11 11:13:19 +0900

    More plots for debugging drive

--- Sketches/TorqueCoilsInvestigate/plot.py
+++ Sketches/TorqueCoilsInvestigate/plot.py
@@ -15,8 +15,30 @@ fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection='polar'))
 
 ax1.plot(data_1[:,1] / 255 * math.pi * 2, data_1[:,2], '.r')
 ax1.plot(data_1[:,1] / 255 * math.pi * 2, data_1[:,3], '.b')
+ax1.set_xlabel("Cycle position")
+ax1.set_title(filename_1)
 
 ax2.plot(data_2[:,1] / 255 * math.pi * 2, data_2[:,2], '.r')
 ax2.plot(data_2[:,1] / 255 * math.pi * 2, data_2[:,3], '.b')
+ax2.set_xlabel("Cycle position")
+ax2.set_title(filename_2)
+
+plt.show()
+
+# %%
+fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection='polar'))
+
+ax1.plot(data_1[:,1] / 255 * math.pi * 2, data_1[:,4], '.r')
+ax1.plot(data_1[:,1] / 255 * math.pi * 2, data_1[:,5], '.b')
+ax1.set_xlabel("Cycle position")
+ax1.set_ylabel("VREF")
+ax1.set_title(filename_1)
+
+ax2.plot(data_2[:,1] / 255 * math.pi * 2, data_2[:,4], '.r')
+ax2.plot(data_2[:,1] / 255 * math.pi * 2, data_2[:,5], '.b')
+ax2.set_xlabel("Cycle position")
+ax2.set_ylabel("VREF")
+ax2.set_title(filename_2)
+
 
 plt.show()
