cdf94e5e828fb303f8ae1440f10583d9d73446b7
Elliot Woods <elliot.woods@gmail.com>
2020-11-12 14:15:57 +0900

    FilteredTarget implemented, fixes #64

--- Firmware/src/Control/PID.h
+++ Firmware/src/Control/PID.h
@@ -1,14 +1,17 @@
 #pragma once
 
-#include "DataTypes.h"
+#include "FilteredTarget.h"
 #include "Utils/FrameTimer.h"
+#include "DataTypes.h"
 
 namespace Control {
 	class PID {
 	public:
+		PID(FilteredTarget &);
 		void init();
 		void update();
 	private:
+		FilteredTarget & filteredTarget;
 		Utils::FrameTimer frameTimer;
 		int64_t priorIntegral = 0;
 		int32_t priorError = 0;
