aeadc98e1168ad218341317583c5a617a641dede
Elliot Woods <elliot.woods@gmail.com>
2020-11-17 22:55:15 +0900

    Fix CANResponer - it wasn't returning ReadRequests properly Add MuscleMemory.NET

--- Firmware/src/Control/FilteredTarget.h
+++ Firmware/src/Control/FilteredTarget.h
@@ -15,6 +15,6 @@ namespace Control {
 		Velocity filterVelocity = 0;
 		MultiTurnPosition priorTarget = 0;
 		uint64_t priorTargetTimestamp = 0;
-		const uint32_t maxTimeDelta = 1 * 1000000; // 1 second
+		const uint32_t maxTimeDelta = 200 * 1000; // 200ms
 	};
 }
---
+++ MuscleMemory.NET/MuscleMemory.NET/Bus.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Candle;
+
+namespace MuscleMemory
+{
+	class Bus
+	{
+		Device FDevice;
+		Channel FChannel;
+
+		public Bus(Device device)
+		{
+			this.FDevice = device;
+			this.Open();
+		}
+
+		~Bus()
+		{
+			this.Close();
+		}
+
+		public void Update()
+		{
+
+		}
+
+		public void Open()
+		{
+			this.FDevice.Open();
+
+			var channels = this.FDevice.Channels;
+			if (channels.Count != 1)
+			{
+				throw (new Exception("This library currently only supports devices with exactly 1 channel"));
+			}
+			this.FChannel = channels.Values.ToList()[0];
+			this.FChannel.Start(500000);
+
+			this.Refresh();
+		}
+
+		public void Close()
+		{
+			if(this.IsOpen)
+			{
+				this.FChannel.Stop();
+				this.FDevice.Close();
+				this.FDevice = null;
+			}
+		}
+
+		public bool IsOpen
+		{
+			get
+			{
+				return this.FDevice != null;
+			}
+		}
+
+		public void Refresh(int timeout = 1000)
+		{
+			// Send the request to all indexes
+			for (int i = 1; i < Messages.MaxIndex; i++)
+			{
+				var message = new Messages.ReadRequest();
+				message.ID = i;
+				message.RegisterType = Messages.RegisterType.DeviceID;
+				this.FChannel.Send(message.Encode());
+			}
+
+			// Wait for responses
+			Thread.Sleep(timeout);
+
+			var replies = this.FChannel.Receive();
+			foreach(var reply in replies)
+			{
+				var message = Messages.Decode(reply);
+				if(message is Messages.ReadResponse)
+				{
+					Console.WriteLine(message);
+				}
+			}
+			Console.WriteLine(replies);
+		}
+	}
+}
