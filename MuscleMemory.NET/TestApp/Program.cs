062e6493289e1e95cf6d41a8c984038290ad8dcc
Elliot Woods <elliot.woods@gmail.com>
2020-11-18 19:15:28 +0900

    Fix CANResponder - it needs to rx betwen tx's MuscleMemory.NET

--- MuscleMemory.NET/TestApp/Program.cs
+++ MuscleMemory.NET/TestApp/Program.cs
@@ -8,6 +8,18 @@ namespace TestApp
 		static void Main(string[] args)
 		{
 			var busGroup = new BusGroup();
+
+			Console.WriteLine("Opening bus...");
+			busGroup.Open(500000);
+
+			Console.WriteLine("Found motors : ");
+			var motors = busGroup.GetAllMotors();
+			foreach(var it in motors)
+			{
+				Console.WriteLine("{0} : {1}", it.Key, it.Value);
+			}
+
+			busGroup.Close();
 		}
 	}
 }
--- MuscleMemory.NET/VVVV.MuscleMemory/BusGroupNode.cs
+++ MuscleMemory.NET/VVVV.MuscleMemory/BusGroupNode.cs
@@ -25,8 +25,14 @@ namespace VVVV.MuscleMemory
 		[Input("Bitrate", IsSingle = true, DefaultValue = 500000)]
 		public IDiffSpread<int> FInBitrate;
 
+		[Input("Enabled", IsSingle = true)]
+		public ISpread<bool> FInEnabled;
+
 		[Output("BusGroup")]
 		public ISpread<BusGroup> FOutBusgroup;
+
+		[Output("Buses")]
+		public ISpread<Bus> FOutBuses;
 
 		[Output("Motor ID")]
 		public ISpread<int> FOutMotorID;
@@ -34,51 +40,51 @@ namespace VVVV.MuscleMemory
 		[Output("Motors")]
 		public ISpread<Motor> FOutMotors;
 
-		[Output("Buses")]
-		public ISpread<Bus> FOutBuses;
-
 		[Output("IsOpen")]
 		public ISpread<bool> FOutIsOpen;
 
 		[Import()]
 		public ILogger FLogger;
 
-		BusGroup FBusGroup = null;
+		BusGroup FBusGroup = new BusGroup();
 		#endregion fields & pins
 
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
-			if ((FInRefresh[0] || FInBitrate.IsChanged) && this.FBusGroup != null)
+			if (FInRefresh[0] || FInBitrate.IsChanged)
 			{
 				this.FBusGroup.Close();
 			}
 
-			if (FBusGroup == null)
-			{
-				this.FBusGroup = new BusGroup(this.FInBitrate[0]);
-			}
-
-			if (!FBusGroup.IsOpen)
+			
+			if (!FBusGroup.IsOpen && FInEnabled[0])
 			{
 				this.FBusGroup.Open(this.FInBitrate[0]);
 			}
+			else if(FBusGroup.IsOpen && !FInEnabled[0])
+			{
+				this.FBusGroup.Close();
+			}
 
 			this.FOutBusgroup[0] = this.FBusGroup;
+			this.FOutIsOpen[0] = this.FBusGroup.IsOpen;
 
-			this.FOutBuses.AssignFrom(this.FBusGroup.Buses);
-
-			var motors = this.FBusGroup.Motors;
-			this.FOutMotorID.AssignFrom(motors.Keys);
-			this.FOutMotors.AssignFrom(motors.Values);
-
-			if(this.FBusGroup == null)
+			if (FBusGroup.IsOpen)
 			{
-				this.FOutIsOpen[0] = false;
+				// This must be called regularly (e.g. once per mainloop frame)
+				this.FBusGroup.Update();
+
+				this.FOutBuses.AssignFrom(this.FBusGroup.Buses);
+				var motors = this.FBusGroup.GetAllMotors();
+				this.FOutMotorID.AssignFrom(motors.Keys);
+				this.FOutMotors.AssignFrom(motors.Values);
 			}
 			else
 			{
-				this.FOutIsOpen[0] = this.FBusGroup.IsOpen;
+				this.FOutBuses.SliceCount = 0;
+				this.FOutMotors.SliceCount = 0;
+				this.FOutMotorID.SliceCount = 0;
 			}
 		}
 	}
