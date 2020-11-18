bd68bef026207a6c1a434d8e1acd5eb2f671f7c2
Elliot Woods <elliot.woods@gmail.com>
2020-11-18 19:15:28 +0900

    Fix CANResponder - it needs to rx betwen tx's MuscleMemory.NET

--- MuscleMemory.NET/MuscleMemory.NET/Motor.cs
+++ MuscleMemory.NET/MuscleMemory.NET/Motor.cs
@@ -6,14 +6,14 @@ namespace MuscleMemory
 {
 	public class Motor
 	{
-		int ID;
+		int FID;
 		Bus FBus;
 		DateTime FLastSeen = DateTime.MinValue;
 		Dictionary<Messages.RegisterType, int> FCachedRegisterValues = new Dictionary<Messages.RegisterType, int>();
 
 		public Motor(int ID, Bus bus)
 		{
-			this.ID = ID;
+			this.FID = ID;
 			this.FBus = bus;
 		}
 
@@ -35,11 +35,40 @@ namespace MuscleMemory
 			this.FBus.Send(readRequest);
 		}
 
-		DateTime LastSeen
+		public void Receive(Messages.ReadResponse readResponse)
+		{
+			this.MarkSeen();
+			this.FCachedRegisterValues[readResponse.RegisterType] = readResponse.Value;
+		}
+
+		public int ID
+		{
+			get
+			{
+				return this.FID;
+			}
+		}
+		public DateTime LastSeen
 		{
 			get
 			{
 				return this.FLastSeen;
+			}
+		}
+
+		public Bus Bus
+		{
+			get
+			{
+				return this.FBus;
+			}
+		}
+
+		public Dictionary<Messages.RegisterType, int> CachedRegisterValues
+		{
+			get
+			{
+				return this.FCachedRegisterValues;
 			}
 		}
 	}
